266837cebb6236d2d10eb3672e65c668e867fde4
Timo Sand <timo.sand@kapsi.fi>
2011-01-24 14:23:37 +0200

    Added boss1

---
+++ boss1/project.clj
@@ -0,0 +1,4 @@
+(defproject boss1 "1.0.0"
+  :description "Clojure 2011 first mid boss"
+  :dependencies [[clojure "1.2.0"]
+                 [midje "1.0.0"]])
---
+++ boss1/src/boss1/core.clj
@@ -0,0 +1,21 @@
+(ns boss1.core)
+
+(def books (load-file "books.clj"))
+
+(defn author-has-years? [book]
+  (cond ))
+
+(defn books-with-author-years [books]
+  ":(")
+
+(defn authors [books]
+  ":(")
+
+(defn author-names [books]
+  ":(")
+
+(defn titles-by-author [author books]
+  ":(")
+
+(defn author-catalog [books]
+  ":(")
