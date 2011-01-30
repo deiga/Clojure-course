85c779ff1c007788e2c8b896067cf95079a376be
Timo Sand <timo.sand@kapsi.fi>
2011-01-30 16:34:36 +0200

    C1 is giving me a headache

--- collections.clj
+++ collections.clj
@@ -1,1 +1,7 @@
 ; Problem C1
+
+(defn doublificate [str-to-num]
+  (let [tmp (map #(str "double-" %) (keys str-to-num))]
+    tmp))
+
+(doublificate {"a" 1 "b" 7}) => {"a" 1 "double-a" 2 "b" 7 "double-b" 14}
