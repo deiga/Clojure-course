1f0133b6f18fb03d2f82ea07557824484afc57ac
Timo Sand <timo.sand@kapsi.fi>
2011-01-30 15:32:15 +0200

    P3 done

--- week2.clj
+++ week2.clj
@@ -9,13 +9,13 @@
     Number  (* 2 arg)
     true))
 
-(generic-doublificate [5 4 2])
-(generic-doublificate '(5 4 2))
-(generic-doublificate 1) => 2
-(generic-doublificate [1 2]) => (2 4)
-(generic-doublificate {}) => nil
-(generic-doublificate []) => nil
-(generic-doublificate {:a 1}) => true
+
+(generic-doublificate 1)        => 2
+(generic-doublificate [1 2])    => (2 4)
+(generic-doublificate '(65 21)) => (130 42)
+(generic-doublificate {})       => nil
+(generic-doublificate [])       => nil
+(generic-doublificate {:a 1})   => true
 
 ; Problem 2
 (defn empty-string? [string]
@@ -29,14 +29,16 @@
 (empty-string? "")         => true
 
 ; Problem 3
-(defn monotonic? [sekvenssi]
-  (let [])
-  (cond
-    (< (first sekvenssi) (second sekvenssi))
-      (map (fn [x y] (== (+ x 1) y)) sekvenssi (next sekvenssi))
-    (> (first sekvenssi) (second sekvenssi))
-      (map (fn [x y] (== (- x 1) y)) sekvenssi (next sekvenssi))))
+(defn pred-and [fst snd]
+  (fn [x] (and (fst x) (snd x))))
 
-(monotonic? [1 2 3]) => true
-(monotonic? [3 2 1]) => true
-(monotonic? [3 2 3]) => false
+(def pos-even? (pred-and even? pos?)) ; here we give the name pos-even? to the function returned by pred-and
+(pos-even? 12) => true
+
+(filter (pred-and even? pos?) [-2 5 12 20]) => (12 20) ; 12 and 20 are the only positive even numbers in the sequence
+
+(def has-title-and-author? (pred-and :title :author))
+(has-title-and-author? {:author "China Miéville" :title "The City and the City"})
+    => "China Miéville" ; The value returned by :author
+(has-title-and-author? {:title "Author unknown"})
+    => nil ; Since :author returns nil
