390edd2898da4a8dcbb0793cf3786a66d7f1cc1d
Timo Sand <timo.sand@kapsi.fi>
2011-01-30 17:33:52 +0200

    Can't get anything done

--- collections.clj
+++ collections.clj
@@ -1,7 +1,16 @@
 ; Problem C1
 
 (defn doublificate [str-to-num]
-  (let [tmp (map #(str "double-" %) (keys str-to-num))]
-    tmp))
+  (let [keys-to-add (map #(str "double-" %) (keys str-to-num))
+        vals-to-add (map #(* 2 %) (vals str-to-num))
+        mapped-addition (zipmap keys-to-add vals-to-add)]
+    (merge-with + str-to-num mapped-addition)))
 
 (doublificate {"a" 1 "b" 7}) => {"a" 1 "double-a" 2 "b" 7 "double-b" 14}
+
+; Problem C2
+(defn halve [input]
+  )
+
+(halve [1 2 3 4])   => [(1 2) (3 4)]
+(halve [1 2 3 4 5]) => [(1 2 3) (4 5)]
