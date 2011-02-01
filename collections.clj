fe1733e9b1b9c2ccd9df2e9cc1208f150e9c3703
Timo Sand <timo.sand@kapsi.fi>
2011-02-01 08:26:46 +0200

    C3 & C4 done

--- collections.clj
+++ collections.clj
@@ -11,7 +11,23 @@
 ; Problem C2
 (defn halve [input]
   (let [half (/ (count input) 2)]
-  (conj [] (take half input) (drop half input))))
+  (split-at half input)))
+  ;(conj [] (take half input) (drop half input))))
 
 (halve [1 2 3 4])   => [(1 2) (3 4)]
 (halve [1 2 3 4 5]) => [(1 2 3) (4 5)]
+
+; Problem C3
+(defn first-five-positives [sequence]
+  (take 5 (filter pos? sequence)))
+
+(first-five-positives [1 3 -2 3 2 0 10 -1 42]) => (1 3 3 2 10)
+(first-five-positives [1 -2 9 -4 -5])          => (1 9)
+
+; Problem C4
+(defn snip [sequence]
+  (conj [] (take-while number? sequence) (drop-while keyword? (drop-while number? sequence))))
+
+(snip [1 4 2 :snip 8 2 9]) => [(1 4 2) (8 2 9)]
+
+; Problem C5
