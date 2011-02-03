39f1055c4d69aef937550bdbde21a816c95fae46
Timo Sand <timo.sand@kapsi.fi>
2011-02-03 16:27:53 +0200

    R6 done

--- recursion.clj
+++ recursion.clj
@@ -62,3 +62,14 @@
 (seq= [1 2 4] '(1 2 4))  => true
 (seq= [1 2 3] [1 2 3 4]) => false
 (seq= [1 3 5] [])        => false
+
+; Problem R6
+(defn power [n k]
+  (if (== 0 k)
+    1
+    (* n (power n (dec k)))))
+
+(power 2 2) => 4
+(power 5 3) => 125
+(power 7 0) => 1
+(power 0 10) => 0
