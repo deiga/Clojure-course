450fc994f1f160161e29e674d73f2477b31ee605
Timo Sand <timo.sand@kapsi.fi>
2011-02-07 21:38:35 +0200

    Manfred von Karma done, updated source changes to tests

--- boss2/src/week2/calculatrix.clj
+++ boss2/src/week2/calculatrix.clj
@@ -2,27 +2,33 @@
 
 (defn read-words []
   "Read a line and split it into words. Returns the words as a vector
-  of strings."
+  of strings. Loops until a line is available."
   (if-let [line (read-line)]
-    (vec (.split line "\\s+"))))
+    (vec (.split line "\\s+"))
+    (recur)))
 
 (defn string->number [string]
   (try
     (Integer/parseInt string)
-    (catch NumberFormatException e nil)))
+    (catch NumberFormatException e nil)
+    (catch ClassCastException e string)))
 
-(defn compute [command args]
+(defn compute [command args & [last-result]]
   "Takes a command and a sequence of arguments, returns computed value
   or nil if given an unknown command or non-integer operands."
   (let [first-operand (string->number (first args))
         second-operand (string->number (second args))]
     (cond
       (< 2 (count args))
-        (str "Too many arguments for command: " command)
-      (nil? first-operand)
+        (str "Wrong number of arguments to " command ": expects 2, you gave " (count args) ".")
+      (and (nil? first-operand) (not (= "_" (first args))))
         (str "Invalid operand: " (first args))
-      (nil? second-operand)
+      (and (nil? second-operand) (not (= "_" (second args))))
         (str "Invalid operand: " (second args))
+      (= "_" (first args))
+        (compute command (conj [] last-result second-operand))
+      (= "_" (second args))
+        (compute command (conj [] first-operand last-result))
       (and first-operand second-operand)
         (case command
           "+" (+ first-operand second-operand)
@@ -30,14 +36,15 @@
           "-" (- first-operand second-operand)
           (str "Invalid command: " command)))))
 
-(defn main []
+(defn main [& [last-result]]
   "This is the driver loop of the calculator. It loops by calling itself recursively."
   (let [words (read-words)
         command (first words)
         arguments (rest words)
-        result (compute command arguments)]
+        result (compute command arguments last-result)]
     (if (or (= "" command) (nil? command))
       "Exiting Calculatrix"
       (do
         (println "  =>" result)
-        (main)))))
+        (main result)))))
+
