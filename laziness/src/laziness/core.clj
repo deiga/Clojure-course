cbee02eca952697d1e6fd847c424308afe29aaaa
Timo Sand <timo.sand@kapsi.fi>
2011-02-08 23:03:08 +0200

    L5 done

--- laziness/src/laziness/core.clj
+++ laziness/src/laziness/core.clj
@@ -17,16 +17,32 @@
   (doseq [num (range up-to)]
     (println (* num num))))
 
-(defn super-composite [n]
-  ":(")
+;; Problem L3
+(defn smallest-divisible []
+  (first (filter #(and (zero? (mod % 72)) (zero? (mod % 108))) (rest (range)))))
+
+;; Problem L4
+(defn divisible-by-all-under? [number up-to]
+  (every? true? (map #(zero? (mod number %)) (range 1 (inc up-to)))))
+
+(defn composite-helper [num up-to]
+  (cond
+    (divisible-by-all-under? num up-to)
+      num
+    :else
+      (composite-helper (inc num) up-to)))
+
+(defn super-composite [n])
 
 (defn indexed
   "Maps elements of a sequence from element to [index-in-sequence element]."
   [s]
   (map vector (range) s))
 
+;; Problem L5
 (defn indexes [a-seq]
-  ":(")
+  (let [with-indices (indexed a-seq)]
+    (map first with-indices)))
 
 (defn inits [a-seq]
   ":(")
