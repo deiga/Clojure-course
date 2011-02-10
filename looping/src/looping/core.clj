0265426dbd931ca45e3e4d792a99c0ffddf47630
Timo Sand <timo.sand@kapsi.fi>
2011-02-10 06:54:48 +0200

    LR1 & LR3 done

--- looping/src/looping/core.clj
+++ looping/src/looping/core.clj
@@ -2,21 +2,30 @@
 
 ;; Problem LR1
 (defn power [base exp]
-  (let [helper (fn [foo bar]
+  (let [helper (fn [foo bar baz]
           (if (zero? bar)
-            foo
-            (recur (* foo foo) (dec bar))))]
-    (helper base (dec exp))))
+            baz
+            (recur foo (dec bar) (* foo baz))))]
+    (helper base exp 1)))
 
 ;; Problem LR2
 (defn last-element [a-seq]
   (when-not (empty? a-seq)
     (if (= 1 (count a-seq))
       (first a-seq)
-      G(recur (drop 1 a-seq)))))
+      (recur (drop 1 a-seq)))))
 
+;; Problem LR2
 (defn seq= [seq1 seq2]
-  ":(")
+    (cond
+      (and (empty? seq1) (empty? seq2))
+        true
+      (or (empty? seq1) (empty? seq2))
+        false
+      (= (first seq1) (first seq2))
+        (recur (rest seq1) (rest seq2))
+      :else
+        false))
 
 (defn find-first-index [pred a-seq]
   ":(")
