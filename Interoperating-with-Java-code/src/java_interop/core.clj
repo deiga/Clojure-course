29bff97658b78e139f22d9835c35f400f647e7e4
Timo Sand <timo.sand@kapsi.fi>
2011-02-20 16:06:03 +0200

    Added fp and interop codes

---
+++ Interoperating-with-Java-code/src/java_interop/core.clj
@@ -0,0 +1,28 @@
+(ns java-interop.core
+  (:import [java.util ArrayList Random Collections Comparator]))
+
+(defn normalize [a-str]
+  ":(")
+
+(defn randoms [seed a-count]
+  ":(")
+
+(defn arraylist-flatten [seq-of-seqs]
+  ":(")
+
+(defn callable-pony []
+  ":(")
+
+(defn minimum-str [strings]
+  ":(")
+
+(defn comparing [function]
+  ":(")
+
+(defn sum-array [array]
+  ":(")
+
+(defn fibo-array [amount]
+  ":(")
+
+;^_^
---
+++ functional-programming/src/functional_programming/core.clj
@@ -0,0 +1,45 @@
+(ns functional-programming.core)
+
+(defn concat-all [a-seq]
+  ":(")
+
+(defn string-cat [a-seq]
+  ":(")
+
+(defn seq-length [a-seq]
+  ":(")
+
+(defn my-reverse [a-seq]
+  ":(")
+
+(defn insert [a-seq elem]
+  ":(")
+
+(defn insertion-sort [a-seq]
+  ":(")
+
+(defn seq-min-max [a-seq]
+  ":(")
+
+(defn parity [a-seq]
+  ":(")
+
+(def my-double
+  (constantly ":("))
+
+(def deep-double
+  (constantly ":("))
+
+(def my-empty?
+  (constantly ":("))
+
+(def find-first
+  (constantly ":("))
+
+(def matrix-sum
+  (constantly ":("))
+
+(def first-result
+  (constantly ":("))
+
+;(
